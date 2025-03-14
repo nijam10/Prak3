a0260ccc9f1ef5e5e31411f65d03c1788e25690b
nijam10 <khainzm61@gmail.com>
2025-03-14 18:55:53 +0700

    Memperbaiki konflik setelah pull

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ListBarangController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/welcome', function (){
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id){
+    return 'User dengan ID ' . $id;
+});
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+});
+
+Route::get('/users', function () {
+    return 'Admin Users';
+});
+});
+
+// Route::get('/listbarang/{id}/{nama}', function ($id, $nama){
+//     return view('list_barang', compact('id', 'nama'));
+//     });
+
+Route::get ('/listbarang{id}/{nama}', [ ListBarangController::class, 'tampilkan']);
